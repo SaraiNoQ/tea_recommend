41287bfa7711763aef9739ded1e5f04acfd7c0b7
SaraiNoQ <2994498974@qq.com>
2022-06-26 02:49:26 +0800

    feat: body category

---
+++ miniprogram/components/bodycategory/bodycategory.ts
@@ -0,0 +1,34 @@
+// components/bodycategory/bodycategory.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tag: {
+      type: String,
+      value: ''
+    },
+    desc: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    navigateToDetail() {
+      wx.navigateTo({
+        url: '/pages/detail/detail'
+      })
+    }
+  }
+})
